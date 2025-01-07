1003f9ef3e0568ce43f0b15074acbad814c1b4a5
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-07 08:54:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 export default function App(){
-  return <div>
-    Hello there
-  </div>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/SignIn" element={<SignIn/>}/>
+      <Route path="/SignUp" element={<SignUp/>}/>
+    </Routes>
+  </BrowserRouter>
 }
