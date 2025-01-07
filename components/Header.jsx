3e8799a2669eab7f72ba84e8a42b7917651669b8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-07 10:55:08 -0500

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 
 import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
 export default function Header(){
+    const path = useLocation().pathname;
+
     return <Navbar className="border-b border-b-slate-200 shadow-lg">
         <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"> 
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500 rounded-lg text-white">
@@ -23,7 +25,7 @@ export default function Header(){
             <AiOutlineSearch/>
         </Button>
         <div className="flex gap-2 md:order-2">
-            <Button className="w-12 h-10 hidden sm:inline" ccolor="gray" pill>
+            <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
                 <FaMoon/>
             </Button>
             <Link
@@ -32,19 +34,20 @@ export default function Header(){
                     Sign In
                 </Button>
             </Link>
+            <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
-                <Navbar.Link>
+                <Navbar.Link active =  {path === "/"} as = {"div"}>
                     <Link to="/">
                         Home
                     </Link>
                 </Navbar.Link>
-                <Navbar.Link>
+                <Navbar.Link active =  {path === "/About"} as = {"div"}>
                     <Link to="/About">
                         About 
                     </Link>
                 </Navbar.Link>
-                <Navbar.Link>
+                <Navbar.Link active =  {path === "/Projects"} as = {"div"}>
                     <Link to="/Projects">
                         Projects
                     </Link>
