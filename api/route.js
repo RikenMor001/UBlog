f9a61244073634e53b24b46b97a076633aa5a856
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-08 23:30:33 -0500

    Update route.js

--- api/route.js
+++ api/route.js
@@ -1,13 +1,47 @@
 
 
 const express = require("express");
+const zod = require("zod");
 const app = express();
 const port = 3000;
 
+const signupBody = zod.object({
+  name: zod.string(),
+  email: zod.string().email(),
+  password: zod.string().min(8),
+})
+
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const body = req.body;
+  if( !body ){
+    return res.status(400).json({
+      message: "The request body is empty"
+    })
+  }
+
+  const exisitingUser = await User.findOne({
+    name: body.name,
+    email: body.email,
+  })
+  if ( exisitingUser ){
+    return res.status(400).json({
+      message: "User already exists"
+    })
+  }
+
+  const user = await User.create({
+    name: body.name,
+    email: body.email,
+    password: body.password,
+  })
+
+  if ( user ){
+    return res.status(200).json({
+      message: "User created successfully"
+    })
+  }
 });
 
 app.post("signin", (req, res) => {
