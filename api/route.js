134d87aa930d1daee83906b03472a762ca8b7ce5
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-09 12:18:46 -0500

    Update route.js

--- api/route.js
+++ api/route.js
@@ -1,53 +1,15 @@
 
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
 
-const express = require("express");
-const zod = require("zod");
 const app = express();
-const port = 3000;
 
-const signupBody = zod.object({
-  name: zod.string(),
-  email: zod.string().email(),
-  password: zod.string().min(8),
+mongoose.connect(process.env.MONGOOSE).then(() => {
+  console.log("Database is connected")
 })
 
-app.use(express.json());
-
-app.post("/signup", async (req, res) => {
-  const body = req.body;
-  if( !body ){
-    return res.status(400).json({
-      message: "The request body is empty"
-    })
-  }
-
-  const exisitingUser = await User.findOne({
-    name: body.name,
-    email: body.email,
-  })
-  if ( exisitingUser ){
-    return res.status(400).json({
-      message: "User already exists"
-    })
-  }
-
-  const user = await User.create({
-    name: body.name,
-    email: body.email,
-    password: body.password,
-  })
-
-  if ( user ){
-    return res.status(200).json({
-      message: "User created successfully"
-    })
-  }
-});
-
-app.post("signin", (req, res) => {
-  const body = req.body;
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")  
 })
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
