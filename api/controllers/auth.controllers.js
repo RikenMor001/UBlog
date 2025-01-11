e1bf71977a52080745be6a983302697094837b15
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-10 21:50:40 -0500

    Update auth.controllers.js

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,26 @@
 
+import User from "../model/user.model.js";
 
 export const signup = async(req, res) => {
-    console.log(req.body)
-    res.json({mssage: "Controller"})
+    const {username, email, password} = req.body;
+    if (!username && !email && !password && username == "" || email == "" || password == "") {
+        res.status(400).json({message: "Please fill all the fields"})
+    }
+
+    const user = await User.findOne({
+        username: username,
+        email: email
+    })
+    if ( user ){
+        res.status(400).json({message: "Username or email already exists"})
+    }  
+
+    const newUser = new User({
+        username,
+        email,
+        password
+    })
+
+    await newUser.save();
+    res.json({message: "User created successfully"})
 }
