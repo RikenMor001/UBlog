7daa0c9386d01f0ec1d17ecf53f214608f0fd98b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-10 21:35:44 -0500

    Update route.js

--- api/route.js
+++ api/route.js
@@ -3,10 +3,13 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
+import authRoute from "./routes/auth.routes.js"
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://riken9377:yadayada@ublog.zkpy8.mongodb.net/UBlog").then(() => {
   console.log("Database is connected")
@@ -17,5 +20,5 @@ app.listen(3000, () => {
 })
 
 app.use("/api/user", userRoute)
-
+app.use("/api/auth", authRoute)
 
