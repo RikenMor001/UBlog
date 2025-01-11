4e776d7316d59f362349195dc64ed76ee71a9d99
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-10 21:06:41 -0500

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,6 +5,15 @@ import express from "express"
 const router = express.Router();
 
 router.get("/test", (req, res) => {
+
+import express from "express"
+import { test } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/test", test) 
+
+export default router
   res.send("Just for testing, api is working")
 }) 
 
