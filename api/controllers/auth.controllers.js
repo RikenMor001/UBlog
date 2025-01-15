73b2590b9b8e41c48e4f13b063b6f60c46aa3fcf
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-14 21:05:13 -0500

    Update auth.controllers.js

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -2,25 +2,31 @@
 import User from "../model/user.model.js";
 
 export const signup = async(req, res) => {
-    const {username, email, password} = req.body;
-    if (!username && !email && !password && username == "" || email == "" || password == "") {
-        res.status(400).json({message: "Please fill all the fields"})
+    try {
+        const {username, email, password} = req.body;
+    
+        if (!username && !email && !password && username == "" || email == "" || password == "") {
+            res.status(400).json({message: "Please fill all the fields"})
+        }
+    
+        const user = await User.findOne({
+            username: username,
+            email: email
+        })
+    
+        if ( user ){
+            res.status(400).json({message: "Username or email already exists"})
+        }  
+    
+        const newUser = new User({
+            username,
+            email,
+            password
+        })
+    
+        await newUser.save();
+        res.json({message: "User Signed up successfully"})
+    } catch (error){
+        console.log("The error is: " + error);
     }
-
-    const user = await User.findOne({
-        username: username,
-        email: email
-    })
-    if ( user ){
-        res.status(400).json({message: "Username or email already exists"})
-    }  
-
-    const newUser = new User({
-        username,
-        email,
-        password
-    })
-
-    await newUser.save();
-    res.json({message: "User created successfully"})
-}
+} 
