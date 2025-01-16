b43928fa63c6a1c1621291362c622c44e24b50a3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-15 23:11:38 -0500

    Update Signup.jsx

--- pages/Signup.jsx
+++ pages/Signup.jsx
@@ -1,10 +1,10 @@
 
-import { Label, TextInput } from "flowbite-react";
+import { Button, Label, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 export default function SignUp(){
   return <div className="min-h-screen mt-20">
-    <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
-      <div>
+    <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+      <div className="flex-1">
       <Link to="/" className=" font-bold dark:text-white text-4xl"> 
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500 rounded-lg text-white">
                 UBlog
@@ -15,8 +15,8 @@ export default function SignUp(){
         </p>   
       </div>
 
-      <div className="">
-        <form>
+      <div className="flex-1">
+        <form className="flex flex-col gap-4 ">
           <div>
             <Label value="Your username"/>
             <TextInput 
@@ -25,7 +25,37 @@ export default function SignUp(){
             id="username"
             />
           </div>
+
+          <div>
+            <Label value="Your email"/>
+            <TextInput 
+            type="text"
+            placeholder="email@email.com"
+            id="email"
+            />
+          </div>
+
+          <div>
+            <Label value="Your password"/>
+            <TextInput 
+            type="password"
+            placeholder="Password"
+            id="password"
+            />
+          </div>
+          
+          <Button gradientDuoTone="purpleToPink" type="submit">
+            Sign up
+          </Button>
         </form>
+        <div className="mt-5">
+          <span>
+            Have an account?
+            <Link to="/signin" className="text-sm text-blue-500 hover:pointer hover:text-blue-700">
+              Sign in
+            </Link>
+          </span>
+        </div>
       </div>
 
     </div>
